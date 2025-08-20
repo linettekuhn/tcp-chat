6d1f4170f2127f6a9fae24227bc6869aa9135d68
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-08-20 00:22:32 -0400

    Switch to HashRouter to host through GitHub Pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import Client from "./pages/Client.tsx";
 import Server from "./pages/Server.tsx";
 
-const router = createBrowserRouter(
-  [
-    { path: "/", element: <App /> },
-    { path: "/server", element: <Server /> },
-    { path: "/client", element: <Client /> },
-  ],
-  { basename: import.meta.env.BASE_URL }
-);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/server" element={<Server />} />
+        <Route path="/client" element={<Client />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
