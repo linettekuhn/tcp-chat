34dbd2bb4c71761d75d4e495489d43d4385b4f21
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-09-03 16:37:44 -0400

    Fix variable name for port

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`app listening on port: ${port}`);
+  console.log(`app listening on port: ${PORT}`);
 });
 
 const serverRouter = require("./routes/server");
