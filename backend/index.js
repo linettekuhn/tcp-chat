b9daabe1c439136fb3e1ad61e6d362f758cc61b4
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-09-03 16:28:37 -0400

    Use Render's assigned port if available

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("app listening on port 3000");
 });
 
